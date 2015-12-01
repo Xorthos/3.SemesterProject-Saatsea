b38ef878156170c30f515aabba28f43ac0c611c7
Xorthos <github@minimads.com>
2015-12-01 09:22:36 +0100

    Work done on the views

--- CustomerManagement/CustomerManagement/Controllers/CustomerController.cs
+++ CustomerManagement/CustomerManagement/Controllers/CustomerController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using CustomerManagement.Models;
 using Proxy.Facade.Abstraction;
 using Proxy.Facade.Implementation;
 
@@ -17,9 +19,21 @@ namespace CustomerManagement.Controllers
             return View(facade.GetCompanyGateway().GetAll());
         }
 
+        [HttpGet]
         public ActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create([Bind(Include = "Name, Zipcode, Email, PhoneNr, Password")] CreateCompanyModel newCompanyModel)
+        {
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Deactivate(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }
